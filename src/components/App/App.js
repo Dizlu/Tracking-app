22b25500ac6bc86c1331e47445318193aed8ddd3
Dizlu <golmarcin@gmail.com>
2017-03-18 17:45:57 +0100

    change readme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,10 @@
 import React from 'react'
-
 /**
  * Main component containing all of the main components
  * Has no connection with store - stateless component
  */
 const App = () => (
   <div>
-    
   </div>
 )
 
