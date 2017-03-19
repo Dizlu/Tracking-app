24246702808eea2187f706735c1f5ca2dec52e6c
Dizlu <golmarcin@gmail.com>
2017-03-19 14:36:15 +0100

    fix broken muiTheme dependency

--- src/containers/AppBarComposition/AppBarComposition.js
+++ src/containers/AppBarComposition/AppBarComposition.js
@@ -1,4 +1,4 @@
-import React, { PropTypes, Component } from 'react'
+import React, { Component } from 'react'
 import AppBar from 'material-ui/AppBar'
 import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
--- src/index.js
+++ src/index.js
@@ -7,27 +7,13 @@ import todoApp from './reducers/index'
 import App from './components/App/App'
 
 
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import { red500, red700, purpleA400 } from 'material-ui/styles/colors'
-
 const store = createStore(todoApp)
-const muiTheme = getMuiTheme({
-  palette: {
-    primary1Color: red500,
-    primary2Color: red700,
-    accent1Color: purpleA400,
-    pickerHeaderColor: red500
-  }
-})
+
 
 render(
   <Provider store={store}>
-    <MuiThemeProvider muiTheme={muiTheme}>
       <App 
-        muiTheme={muiTheme}
       />
-    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root')
 )
