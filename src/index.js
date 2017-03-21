a5ac6044ce351355074a86390fc3e37830ff5500
Dizlu <golmarcin@gmail.com>
2017-03-21 23:47:44 +0100

    working react-router. need to rebuild structure of app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { createBrowserHistory } from 'history';
 import App from './components/App/App'
 import store from './store'
-
 import './index.css'
+
+const history = syncHistoryWithStore(createBrowserHistory(), store)
 
 ReactDOM.render(
   <Provider store={store}>
-      <App />
+    <Router history={history} >
+      <Route path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
--- src/reducers/mainReducer.js
+++ src/reducers/mainReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import todoData from './todoData/todoData'
+import { routerReducer } from 'react-router-redux'
 
 const mainReducer = combineReducers({
-  todoData
+  todoData,
+  routing: routerReducer
 })
 
 export default mainReducer
