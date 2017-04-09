0f1faad1ae6bfa53a2dba4be25c23b85495f4086
Dizlu <golmarcin@gmail.com>
2017-04-09 10:06:30 +0200

    hotfix- disable toggle, fixing delete

--- src/components/ListTodos.js
+++ src/components/ListTodos.js
@@ -1,30 +1,11 @@
 import React from 'react'
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table'
-import FloatingActionButton from 'material-ui/FloatingActionButton'
 import Delete from 'material-ui/svg-icons/action/delete'
-
-const styles = {
-  mediumIcon: {
-    width: 48,
-    height: 48,
-  },
-  medium: {
-    width: 96,
-    height: 96,
-    padding: 24,
-  }
-}
 
 const ListTodos = ({todoList = [], onToggle, onDelete}) =>
   (
     <Table
-      onCellClick={(cellNumber, cellColumn) => {
-        console.log(cellColumn)
-        if (!cellColumn) {
-          onToggle(todoList[cellNumber].id)
-        }
-      }}
-      multiSelectable={true}>
+      selectable={false}>
       <TableHeader
         displaySelectAll={false}
       >
@@ -32,21 +13,18 @@ const ListTodos = ({todoList = [], onToggle, onDelete}) =>
           <TableHeaderColumn>DESCRIPTION</TableHeaderColumn>
         </TableRow>
       </TableHeader>
-      <TableBody
-        showRowHover={true}
-        deselectOnClickaway={false}
-      >
+      <TableBody displayRowCheckbox={false} showRowHover={true}>
         {todoList
           .map((todo) => {
             return (
-              <TableRow
-                selected={todo.completed}
-                key={todo.id}>
+              <TableRow selected={todo.completed} key={todo.id}>
                 <TableRowColumn>
                   {todo.text}
                 </TableRowColumn>
                 <TableRowColumn>
-                  <Delete onClick={() => {
+                  <Delete onClick={(e) => {
+                    e.stopPropagation()
+                    e.nativeEvent.stopImmediatePropagation()
                     onDelete(todo.id)
                   }}/>
                 </TableRowColumn>
