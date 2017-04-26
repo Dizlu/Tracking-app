d69d1a7a531009eb5d37c9a41b656b7b7cf56b98
Dizlu <golmarcin@gmail.com>
2017-04-26 22:35:47 +0200

    remove trailign test

--- tests/components/App.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from './../../src/components/App'
-xit('renders without crashing', () => {
-  ReactDOM.render(<App />, div)
-})
